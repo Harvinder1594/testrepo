a0fa3a6a4d5e4caa2a428f1cc5e406c9872085b4
Harvinder1594 <165412264+Harvinder1594@users.noreply.github.com>
2024-04-01 01:04:35 +0530

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,3 +1,5 @@
 # Display the output
 print("my new python file")
 
+link of submission on coursera project- Stock analysis using Yfinance
+link---   https://www.coursera.org/learn/python-project-for-data-science/peer/WU7xb/analyzing-historical-stock-revenue-data-and-building-a-dashboard/review/5OU5Ve-UEe6slA6kuckxvw
